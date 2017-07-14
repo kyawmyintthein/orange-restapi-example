ee7e412b99f17d8533c3f61335e49031fe5d73d3
Kyaw Myint Thein <kyawmyintthein2020@gmail.com>
2017-07-14 16:45:33 +0800

    Add config

--- server.go
+++ server.go
@@ -2,7 +2,7 @@ package main
 
 import "github.com/kyawmyintthein/orange"
 import "net/http"
-// import "log"
+import "log"
 var App *orange.App
 var ns_v1 *orange.Router
 
@@ -12,7 +12,19 @@ func main() {
 
 func init() {
 	App = orange.NewApp("Test")
-	// log.Printf("App %+v", App)
+	log.Printf("Default ENV %+v \n", App.ENV())
+	config := App.AppConfig()
+	log.Printf("Config %+v \n", config)
+	log.Printf("App Name %+v \n", config.GetString("app.name"))
+	var dbConfig *orange.Config
+	var err error
+	if dbConfig, err = App.NewConfig("db", "", "json"); err != nil{
+		log.Printf("Error " + err.Error())
+	}
+
+	log.Printf("Config %+v \n", dbConfig)
+	log.Printf("DEV database %+v \n", dbConfig.GetString("dev.database.name"))
+
 	ns_v1 = App.Namespace("/v1")
 	var objectController = ns_v1.Controller("/objects")
 	objectController.GET("/", func(ctx *orange.Context) {
